4a3e4688fe8cae56007e0eec99967e8fdb2d8dd3
abraraltaf92 <abraraltaf92@icloud.com>
2020-12-11 03:11:35 +0530

    spartan_level added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home: SpartanId()));
 
-class SpartanId extends StatelessWidget {
+class SpartanId extends StatefulWidget {
+  @override
+  _SpartanIdState createState() => _SpartanIdState();
+}
+
+class _SpartanIdState extends State<SpartanId> {
+  int spartanLevel = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +24,46 @@ class SpartanId extends StatelessWidget {
         centerTitle: true,
         elevation: 0.0,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  if (spartanLevel > 0) {
+                    spartanLevel -= 1;
+                  }
+                });
+              },
+              child: Icon(Icons.remove),
+              backgroundColor: Colors.grey[800],
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  spartanLevel += 1;
+                });
+              },
+              child: Icon(Icons.add),
+              backgroundColor: Colors.grey[800],
+            )
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     setState(() {
+      //       spartanLevel += 1;
+      //     });
+      //   },
+      //   child: Icon(
+      //     Icons.add,
+      //   ),
+      //   backgroundColor: Colors.grey[800],
+      // ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
@@ -29,7 +76,7 @@ class SpartanId extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 60,
+              height: 40,
             ),
             Text(
               "Name",
@@ -70,6 +117,30 @@ class SpartanId extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
+            SizedBox(height: 20),
+            Text(
+              "Spartan Level",
+              style: TextStyle(
+                color: Colors.grey[400],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50.0, 0, 0, 0),
+              child: Text(
+                "$spartanLevel",
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25.0,
+                  fontFamily: 'DoHyeon',
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ),
             SizedBox(height: 30),
             Row(
               children: [
@@ -91,9 +162,6 @@ class SpartanId extends StatelessWidget {
                   ),
                 ),
               ],
-            ),
-            SizedBox(
-              height: 10,
             ),
           ],
         ),
